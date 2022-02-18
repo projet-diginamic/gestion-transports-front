6f0700219bd941b4f10965f5f1755cc0b08b4c42
Fertus ANTOINETTE <fertus.antoinette@izus.fr>
2022-02-18 16:17:28 +0100

    Ajout bundle bootstrap js et ajout du code du composant titre

--- src/app/composants/titre-page-component/titre-page-component.component.ts
+++ src/app/composants/titre-page-component/titre-page-component.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
 
 @Component({
   selector: 'app-titre-page-component',
@@ -6,10 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./titre-page-component.component.scss']
 })
 export class TitrePageComponentComponent implements OnInit {
+  
+  /**
+   ***********************************************************************************************    
+   * 
+   *    Composant titre page
+   * 
+   *********************************************************************************************** 
+   *    Fonction du composant :
+   *      - Gestion de l'affichage du titre
+   *      - Gestion du bouton d'action associé 
+   * 
+   *      Input :
+   *        - titrePage:string => titre de la page
+   *        - classBtn:string => class bootstrap que l'on souhaite associer au bouton
+   *        - libelleBtn:string => Libellé du bouton
+   *        - btnClickReturn:string => message attendu lors de l'évènement click
+   * 
+   */
+
+  @Input() titrePage:string = "Titre de la page";
+  @Input() libelleBtn:string = "Libellé du bouton";
+  @Input() btnClickReturn:string = "event";
+  @Input() classBtn:string = "btn-outline-success";
+  @Output() action:EventEmitter<string> = new EventEmitter<string>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  executerAction(){
+    this.action.emit(this.btnClickReturn);
+  }
+
+  ajouterStyleBtn(){
+    return this.classBtn;
   }
 
 }
